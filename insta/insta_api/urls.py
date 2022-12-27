0010d41a86aa808d110ba246befc3930f227c35c
ryglu <rygielpiotr18@gmail.com>
2022-12-28 00:09:45 +0100

    Added some new endpoints and get, put, delete methods

--- insta/insta_api/urls.py
+++ insta/insta_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import(
     PhotoListApiView,
+    PhotoDetailApiView,
     CommentListApiView,
     CommentDetailApiView,
+    CommentDetailApiView2,
     LikeListApiView,
     LikeDetailApiView,
     homePageView
@@ -10,8 +12,10 @@ from .views import(
 
 urlpatterns = [
     path('photos', PhotoListApiView.as_view()),
+    path('photos/<int:photo_id>/', PhotoDetailApiView.as_view()),
     path('comments', CommentListApiView.as_view()),
     path('comments/<int:photo_id>/', CommentDetailApiView.as_view()),
+    path('comments/<int:comment_id>/comment/', CommentDetailApiView2.as_view()),
     path('likes', LikeListApiView.as_view()),
     path('likes/<int:photo_id>/', LikeDetailApiView.as_view()),
     path('homepage', homePageView, name="home")
--- insta/insta_api/views.py
+++ insta/insta_api/views.py
@@ -13,7 +13,7 @@ class PhotoListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        insta_posts = Photo.objects.filter(user=request.user.id)
+        insta_posts = Photo.objects
         serializer = PhotoSerializer(insta_posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -30,12 +30,60 @@ class PhotoListApiView(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class PhotoDetailApiView(APIView):
+    permision_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, photo_id, *args, **kwargs):
+        try:
+            photo_instance = Photo.objects.get(id=photo_id)
+        except Photo.DoesNotExist:
+            return Response(
+                {"res": "Photo with photo_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = PhotoSerializer(photo_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, photo_id, *args, **kwargs):
+        try:
+            photo_instance = Photo.objects.get(id=photo_id, user=request.user.id)
+        except Photo.DoesNotExist:
+            return Response(
+                {"res": "Photo with photo_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'content': request.data.get('content'),
+            'user': request.user.id,
+            'username': request.user.username
+        }
+        serializer = PhotoSerializer(instance=photo_instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, photo_id, *args, **kwargs):
+        try:
+            photo_instance = Photo.objects.get(id=photo_id, user=request.user.id)
+        except Photo.DoesNotExist:
+            return Response(
+                {"res": "Photo with photo_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        photo_instance.delete()
+        return Response(
+            {"res": "Photo deleted!"},
+            status=status.HTTP_200_OK
+        )
+
 
 class CommentListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        comments = Comment.objects.filter(user_id=request.user.id)
+        comments = Comment.objects
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -67,16 +115,65 @@ class CommentDetailApiView(APIView):
     '''
 
     def get(self, request, photo_id, *args, **kwargs):
+        # returns all comments under photo with given photo_id
         comments_instance = Comment.objects.filter(photo_id=photo_id)
         if not comments_instance:
             return Response(
-                {"res": "Object with photo_id does not exists"},
+                {"res": "Comments with photo_id does not exists"},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
         serializer = CommentSerializer(comments_instance, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class CommentDetailApiView2(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, comment_id, *args, **kwargs):
+        try:
+            comment_instance = Comment.objects.get(id=comment_id)
+        except Comment.DoesNotExist:
+            return Response(
+                {"res": "Comment with comment_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = CommentSerializer(comment_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, comment_id, *args, **kwargs):
+        # updates conent of comment with given comment_id
+        try:
+            comment_instance = Comment.objects.get(id=comment_id, user_id=request.user.id)
+        except Comment.DoesNotExist:
+            return Response(
+                {"res": "Comment with comment_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'content': request.data.get('content'),
+            'user_id': request.user.id,
+        }
+        serializer = CommentSerializer(instance=comment_instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, comment_id, *args, **kwargs):
+        try:
+            comment_instance = Comment.objects.get(id=comment_id, user_id=request.user.id)
+        except Comment.DoesNotExist:
+            return Response(
+                {"res": "Comment with comment_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        comment_instance.delete()
+        return Response(
+            {"res": "Comment deleted!"},
+            status=status.HTTP_200_OK
+        )
 
 class LikeListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
