8d5454cf94a029754485c7a6fab3703424315816
ryglu <rygielpiotr18@gmail.com>
2023-01-08 22:44:15 +0100

    Added new endpoints, edited existed endpoints, fixed bugs

--- insta/insta_api/urls.py
+++ insta/insta_api/urls.py
@@ -4,19 +4,21 @@ from .views import(
     PhotoDetailApiView,
     CommentListApiView,
     CommentDetailApiView,
-    CommentDetailApiView2,
     LikeListApiView,
     LikeDetailApiView,
+    FollowListApiView,
+    FollowDetailedApiView,
     homePageView
 )
 
 urlpatterns = [
     path('photos', PhotoListApiView.as_view()),
     path('photos/<int:photo_id>/', PhotoDetailApiView.as_view()),
-    path('comments', CommentListApiView.as_view()),
-    path('comments/<int:photo_id>/', CommentDetailApiView.as_view()),
-    path('comments/<int:comment_id>/comment/', CommentDetailApiView2.as_view()),
-    path('likes', LikeListApiView.as_view()),
-    path('likes/<int:photo_id>/', LikeDetailApiView.as_view()),
+    path('photos/<int:photo_id>/comments', CommentListApiView.as_view()),
+    path('photos/<int:photo_id>/likes', LikeListApiView.as_view()),
+    path('comments/<int:comment_id>/', CommentDetailApiView.as_view()),
+    path('likes/<int:like_id>/', LikeDetailApiView.as_view()),
+    path('users/<int:following_id>/followers', FollowListApiView.as_view()),
+    path('follows/<int:follow_id>/', FollowDetailedApiView.as_view()),
     path('homepage', homePageView, name="home")
 ]
--- insta/insta_api/views.py
+++ insta/insta_api/views.py
@@ -3,10 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+from django.db.models import Q
 # from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .models import Photo, Comment, Like
-from .serializers import PhotoSerializer, CommentSerializer, LikeSerializer
+from .models import Photo, Comment, Like, Follow
+from .serializers import PhotoSerializer, CommentSerializer, LikeSerializer, FollowSerializer
 
 
 class PhotoListApiView(APIView):
@@ -82,38 +83,6 @@ class PhotoDetailApiView(APIView):
 class CommentListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request, *args, **kwargs):
-        comments = Comment.objects
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        data = {
-            'content': request.data.get('content'),
-            'user_id': request.user.id,
-            'photo_id': request.data.get('photo_id')
-        }
-        serializer = CommentSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CommentDetailApiView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    '''
-    def get_comments(self, photo_id):
-
-        Helper method to get the objects (comments) with given photo_id
-
-        try:
-            return Comment.objects.filter(photo_id=photo_id)
-        except Comment.DoesNotExist:
-            return None
-    '''
-
     def get(self, request, photo_id, *args, **kwargs):
         # returns all comments under photo with given photo_id
         comments_instance = Comment.objects.filter(photo_id=photo_id)
@@ -126,8 +95,21 @@ class CommentDetailApiView(APIView):
         serializer = CommentSerializer(comments_instance, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request, photo_id, *args, **kwargs):
+        data = {
+            'content': request.data.get('content'),
+            'user_id': request.user.id,
+            'photo_id': photo_id
+        }
+        serializer = CommentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-class CommentDetailApiView2(APIView):
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, comment_id, *args, **kwargs):
@@ -178,22 +160,6 @@ class CommentDetailApiView2(APIView):
 class LikeListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request, *args, **kwargs):
-        data = {
-            'user_id': request.user.id,
-            'photo_id': request.data.get('photo_id')
-        }
-        serializer = LikeSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LikeDetailApiView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
     def get(self, request, photo_id, *args, **kwargs):
         likes_instance = Like.objects.filter(photo_id=photo_id)
         if not likes_instance:
@@ -205,6 +171,89 @@ class LikeDetailApiView(APIView):
         serializer = LikeSerializer(likes_instance, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request, photo_id, *args, **kwargs):
+        data = {
+            'user_id': request.user.id,
+            'photo_id': photo_id
+        }
+        check = Like.objects.filter(Q(user_id=request.user.id) & Q(photo_id=photo_id))
+        if check.exists():
+            return Response(
+                {"res": "Already liked"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = LikeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LikeDetailApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, like_id, *args, **kwargs):
+        try:
+            like_instance = Like.objects.get(id=like_id, user_id=request.user.id)
+        except Like.DoesNotExist:
+            return Response(
+                {"res": "Like with like_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        like_instance.delete()
+        return Response(
+            {"res": "Like deleted!"},
+            status=status.HTTP_200_OK
+        )
+
+
+class FollowListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, following_id, *args, **kwargs):
+        follows_instance = Follow.objects.filter(following_id=following_id)
+        if not follows_instance:
+            return Response(
+                {"res": "Object with following_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = FollowSerializer(follows_instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, following_id, *args, **kwargs):
+        data = {
+            'follower_id': request.user.id,
+            'following_id': following_id
+        }
+        check = Follow.objects.filter(Q(follower_id=request.user.id) & Q(following_id=following_id))
+        if check.exists():
+            return Response(
+                {"res": "You already follow this user"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = FollowSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FollowDetailedApiView(APIView):
+    def delete(self, request, follow_id, *args, **kwargs):
+        try:
+            follow_instance = Follow.objects.get(id=follow_id, follower_id=request.user.id)
+        except Follow.DoesNotExist:
+            return Response(
+                {"res": "Follow with follow_id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        follow_instance.delete()
+        return Response(
+            {"res": "Follow deleted!"},
+            status=status.HTTP_200_OK
+        )
 
 # def homePageView(request):
 #     return HttpResponse(Like.objects.filter(photo_id=3).count())
